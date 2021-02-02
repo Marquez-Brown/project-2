ceb3469e66f5dc823f84a70ac5b93aab6de4d486
Paul Keldsen <Pkeld148@gmail.com>
2021-02-02 05:16:36 -0500

    got basically everything working, sup.

--- models/songs.js
+++ models/songs.js
@@ -2,7 +2,10 @@ module.exports = function (sequelize, DataTypes) {
   const Song = sequelize.define("Song", {
     title: DataTypes.STRING,
     artist: DataTypes.STRING,
-    rating: DataTypes.INTEGER,
+    rating: {
+      type: DataTypes.INTEGER,
+      defaultValue: "0",
+    },
     total_ratings: DataTypes.INTEGER,
     // Depending on how the API works, might not need "url" but putting in for now
     url: DataTypes.STRING,
